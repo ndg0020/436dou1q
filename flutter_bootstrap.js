dd21498bdb999bfde04730fab866bac87933b3d6
Nicolò <ndg0029@gmail.com>
2026-04-19 02:49:37 +0200

    redeploy

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3263032958" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3303672555" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
